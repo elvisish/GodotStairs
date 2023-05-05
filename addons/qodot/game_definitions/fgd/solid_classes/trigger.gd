ecd4a94006b444268129cc5eb1017d5429f90b08
elvisish <16231628+elvisish@users.noreply.github.com>
2023-05-05 23:52:07 +0100

    updated to godot 4.0

--- addons/qodot/game_definitions/fgd/solid_classes/trigger.gd
+++ addons/qodot/game_definitions/fgd/solid_classes/trigger.gd
@@ -3,10 +3,11 @@ extends Node
 signal trigger()
 
 func _ready():
-	connect("body_entered", self, "handle_body_entered")
+#	connect("body_entered", self, "handle_body_entered")
+	self.body_entered.connect(handle_body_entered)
 
 func handle_body_entered(body: Node):
-	if body is StaticBody:
+	if body is StaticBody3D:
 		return
 
 	emit_signal("trigger")
